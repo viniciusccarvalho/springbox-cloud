c19e0bf84b9aee4155426a39acfa39d022235d8a
Dave Syer <dsyer@pivotal.io>
2016-04-19 12:38:55 +0100

    Fix auth server to use normal HTTP basic

--- springbox-application/src/main/java/io/springbox/apigateway/SpringboxApplication.java
+++ springbox-application/src/main/java/io/springbox/apigateway/SpringboxApplication.java
@@ -3,9 +3,8 @@ package io.springbox.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 /**
@@ -14,7 +13,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableOAuth2Sso
 public class SpringboxApplication {
 
 	public static void main(String[] args) {
